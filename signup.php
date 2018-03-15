76ce16ef2f6172e8d357630199bfbdafd3d9d09e
iroshmaya <37334513+iroshmaya@users.noreply.github.com>
2018-03-15 09:44:39 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,11 +5,33 @@ include_once 'header.php';
 		<div class="main-wrapper">
 		<h2>Signup</h2>
 		<form class="signup-form" action="includes/Signup.inc.php" method="POST">
+			
+			<div class="inputWithIcon">
+			<i class="fa fa-user fa-lg fa-fw" aria.hidden="true">   </i>
 			<input type="text" name="first" placeholder="Firstname">
+			</div>
+			
+			<div class="inputWithIcon">
+			<i class="fa fa-user fa-lg fa-fw" aria.hidden="true">   </i>
 			<input type="text" name="last" placeholder="Lastname">
+			</div>
+			
+			<div class="inputWithIcon">
+			<i class="fa fa-envelope fa-lg fa-fw" aria.hidden="true">   </i>
 			<input type="text" name="email" placeholder="E-mail">
+			</div>
+			
+			<div class="inputWithIcon">
+			<i class="fa fa-user fa-lg fa-fw" aria.hidden="true">   </i>
 			<input type="text" name="uid" placeholder="Username">
+			</div>
+			
+			<div class="inputWithIcon">
+			<i class="fa fa-key fa-lg fa-fw" aria.hidden="true">   </i>
 			<input type="text" name="pwd" placeholder="Password">
+			</div>
+			
+			
 			<button type="submit" name="submit">Sign up</button>
 		</form>
 		</div>
